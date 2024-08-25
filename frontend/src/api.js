fee3013c6be5b4607055ec06351f3bce7d1ee83b
Safwan <mohdsafwan2k5@gmail.com>
2024-08-26 00:17:01 +0500

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const apiUrl =
+   "https://16d6c3fe-7242-4453-af5c-b75e3719c75c-dev.e1-eu-north-azure.choreoapis.dev/djangoreact/backend/v1";
+
 const api = axios.create({
-   baseURL: import.meta.env.VITE_API_URL,
+   baseURL: import.meta.env.VITE_API_URL
+      ? import.meta.env.VITE_API_URL
+      : apiUrl,
 });
 
 api.interceptors.request.use(
